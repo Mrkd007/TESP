b77856fb3cf3004553d0baafc5f16bbe3ae5db23
vipin-developer <vipinrajandcompany@gmail.com>
2018-07-07 22:42:23 +0530

    Revert "Initial Commit"

--- Angular/connection.php
+++
@@ -1,7 +0,0 @@
-<?php 
-define("HOSTNAME", "localhost");
-define("USERNAME", "root");
-define("PASSWORD", "");
-define("DATABASE", "angulartest");
-$dbhandle=new mysqli(HOSTNAME,USERNAME,PASSWORD,DATABASE)or die("could not connect ");
-?>
--- Angular/insert.php
+++
@@ -1,8 +0,0 @@
-<<?php 
-include "connection.php";
-$data=json_decode(file_get_contents("php://input"));
-$id=$dbhandle->real_escape_string($data->id);
-$name=$dbhandle->real_escape_string($data->name);
-$query="INSERT INTO angular VALUES($id,'".$name."')";
-$dbhandle->query($query);
- ?>
